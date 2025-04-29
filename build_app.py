3856d2cbe4f7c6fb403f7fc19bcb235f827be5e1
HL <415.lo.hubert@gmail.com>
2025-04-29 21:13:31 +0000

    Added error message

--- build_app.py
+++ build_app.py
@@ -11,6 +11,9 @@ from sklearn.metrics import r2_score, mean_squared_error # type: ignore
 uploaded_file = st.file_uploader("Upload the healthcare data file")
 df = pd.read_csv(uploaded_file)
 
+
+st.write("Ignore Errors Below will be removed After file is inserted")
+
 # Programmatically set the config to hide error details
 st.set_option('client.showErrorDetails', False)
 
